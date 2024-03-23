b56bba7a8c39ebc0becd54b59430f25938da55ba
Victor Candido <victor.candido@iv2.com.br>
2024-03-23 12:21:50 -0300

    UserButton

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { UserButton } from "@/components/auth/user-button";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { usePathname } from "next/navigation"
 
 export const Navbar = () => {
     const pathname = usePathname();
